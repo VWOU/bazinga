9c92c8cbd6c36a6212cefd98d5089a2b78bbce66
Vincent <vincent.wouters@youconsulting.com>
2013-03-06 15:30:01 +0100

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bazinga::Application.config.secret_token = '6398e816298a8e93e588820a40f29edfc02528d88fdb711f9b8e89cca70c8a3a3a6b9e17390872cf2511cde1c753b94ef81306d410dfa991691a13a006f09519'
